708aa4a530cd47231dc78bf9cf9bd7f8b0078e2e
AlekseyBuyankin <aleksey77456@mail.ru>
2020-04-11 14:30:31 +0300

    before json

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled>Quiz</b-nav-item>
+      <b-nav-item disabled>Total 0/10</b-nav-item>
+      <b-nav-item disabled>Counter 0/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>
+        Questions
+      </template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item>Answers</b-list-group-item>
+        <b-list-group-item>Answers</b-list-group-item>
+        <b-list-group-item>Answers</b-list-group-item>
+        <b-list-group-item>Answers</b-list-group-item>
+      </b-list-group>
+
+      <hr class="my-4" />
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<style scoped>
+.jumbotron {
+  padding: 2rem 2rem;
+}
+
+.btn {
+  margin: 0 5px;
+}
+
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+</style>
